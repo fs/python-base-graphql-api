dd8b83536659c76240d7e6834dae823553d0c82d
RamilAbzalov <ramil.abzalov@scalewill.com>
2021-07-29 16:00:10 +0300

    added email templates

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -1,66 +1,58 @@
-"""
-Django settings for server project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/topics/settings/
-
-For the full list of settings and their config, see
-https://docs.djangoproject.com/en/3.2/ref/settings/
-"""
-
 from datetime import timedelta
-from typing import Dict, List, Tuple, Union
+from pathlib import Path
 
-from django.utils.translation import gettext_lazy as _
-from server.settings.components import BASE_DIR, config
+from decouple import config
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-# Application definition:
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-INSTALLED_APPS: Tuple[str, ...] = (
-    # Your apps go here:
-    'server.apps.users',
-    'server.core.authentication.jwt',
-    'server.core.graphql',
+ALLOWED_HOSTS = ['localhost', config('DOMAIN_NAME')]
 
-    # Default django apps:
+INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # graphene
     'graphene_django',
     'django_filters',
+    'server.core.authentication.jwt',
+    'server.apps.users',
+]
 
-    # django-admin:
-    'django.contrib.admin',
-    'django.contrib.admindocs',
-)
-
-MIDDLEWARE: Tuple[str, ...] = (
-
-    # Django:
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # django-permissions-policy
-    'django_permissions_policy.PermissionsPolicyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 ROOT_URLCONF = 'server.urls'
 
-WSGI_APPLICATION = 'server.wsgi.application'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR.joinpath('templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'base_graphql_api.wsgi.application'
 
 
 # Database
@@ -68,19 +60,36 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('POSTGRES_DB'),
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': config('POSTGRES_HOST'),
-        'PORT': config('POSTGRES_PORT', cast=int),
-        'CONN_MAX_AGE': config('CONN_MAX_AGE', cast=int, default=60),
-        'OPTIONS': {
-            'connect_timeout': 10,
-            'options': '-c statement_timeout=15000ms',
-        },
-    },
+        'PORT': config('POSTGRES_PORT'),
+    }
 }
+
+AUTH_USER_MODEL = 'users.User'
+
+AUTHENTICATION_BACKENDS = [
+    'users.jwt_auth.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 
 # Internationalization
@@ -88,85 +97,25 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
+TIME_ZONE = 'Europe/Moscow'
+
 USE_I18N = True
+
 USE_L10N = True
 
-LANGUAGES = (
-    ('en', _('English')),
-    ('ru', _('Russian')),
-)
-
-LOCALE_PATHS = (
-    'locale/',
-)
-
 USE_TZ = True
-TIME_ZONE = 'UTC'
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('static')
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-
-# Templates
-# https://docs.djangoproject.com/en/3.2/ref/templates/api
-
-TEMPLATES = [{
-    'APP_DIRS': True,
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [
-        # Contains plain text templates, like `robots.txt`:
-        BASE_DIR.joinpath('server', 'templates'),
-    ],
-    'OPTIONS': {
-        'context_processors': [
-            # Default template context processors:
-            'django.contrib.auth.context_processors.auth',
-            'django.template.context_processors.debug',
-            'django.template.context_processors.i18n',
-            'django.template.context_processors.media',
-            'django.contrib.messages.context_processors.messages',
-            'django.template.context_processors.request',
-        ],
-    },
-}]
-
-
-# Media files
-# Media root dir is commonly changed in production
-# (see development.py and production.py).
-# https://docs.djangoproject.com/en/3.2/topics/files/
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.joinpath('media')
-
-
-# Django authentication system
-# https://docs.djangoproject.com/en/3.2/topics/auth/
-
-AUTH_USER_MODEL = 'users.User'
-
-AUTHENTICATION_BACKENDS = (
-    'server.core.authentication.jwt.backends.JSONWebTokenBackend',
-    'django.contrib.auth.backends.ModelBackend',
-)
-
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-]
-
-PASS_RESET_TOKEN_EXPIRATION_DELTA = timedelta(days=1)
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 JWT_SETTINGS = {
     'REFRESH_TOKEN_EXPIRATION_DELTA': timedelta(days=30),
@@ -180,28 +129,17 @@ JWT_SETTINGS = {
     'JWT_VERIFY': True,
 }
 
-# Timeouts
-# https://docs.djangoproject.com/en/3.2/ref/settings/#std:setting-EMAIL_TIMEOUT
+AWS_ACCESS_KEY_ID = config('S3_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('S3_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('S3_BUCKET_NAME')
+AWS_S3_REGION_NAME = config('S3_BUCKET_REGION')
+AWS_S3_MAX_MEMORY_SIZE = 10 * 1024 * 1024
 
-EMAIL_TIMEOUT = 5
+PASS_RESET_TOKEN_EXPIRATION_DELTA = timedelta(days=1)
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
-# Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-
-
-# AWS S3 Storage
-AWS_ACCESS_KEY_ID = config('S3_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('S3_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('S3_BUCKET_NAME')
-AWS_S3_REGION_NAME = config('S3_BUCKET_REGION')
-AWS_S3_MAX_MEMORY_SIZE = 10 * 1024 * 1024
