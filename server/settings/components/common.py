2f77a97760ddce98fa0ee747a7c5cd76900ce28f
timuralchin <timur.alchin@flatstack.com>
2021-08-03 11:31:50 +0300

    Added django-redis

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -41,7 +41,20 @@ INSTALLED_APPS: Tuple[str, ...] = (
     # django-admin:
     'django.contrib.admin',
     'django.contrib.admindocs',
+
+    # redis
+    'store',
 )
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 MIDDLEWARE: Tuple[str, ...] = (
 
